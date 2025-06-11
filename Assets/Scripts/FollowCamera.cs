96212cc151036031a1190d5bac489a648d6a39c7
Christian Schneider <fuzzyherbivore@icloud.com>
2025-06-11 15:33:11 +0200

    Made camera follow player with a fixed offset with movement smoothing.

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] GameObject targetObject;
+    [SerializeField] Vector3 offsetFromObject = new(0, 10, -10);
+    [SerializeField] float smoothTime;
+
+    Vector3 currentVelocity;
+
+    void LateUpdate()
+    {
+        if (targetObject == null) return;
+
+        transform.position = Vector3.SmoothDamp(
+            transform.position,
+            targetObject.transform.position + offsetFromObject,
+            ref currentVelocity,
+            smoothTime
+        );
+    }
+}
