530885f72abbda7ffbef29e749254c1ebde0ef35
Gerald Schmidtke <gerald.schmidtke@s4g.school4games.net>
2025-06-12 14:14:06 +0200

    begun work on weapon demo

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject projectilePrefab;
+    [SerializeField] float projectileSpeed;
+    [SerializeField] Transform player;
+
+    [SerializeField] float shootInterval;
+    [SerializeField] float attackRange = 5f;
+    float timeToShoot;
+
+    float AttackReady
+    {
+        get
+        {
+            timeToShoot <= 0f;
+        }
+    }
+
+    void Shoot(Vector3 direction)
+    {
+        var projectile = Instantiate(projectilePrefab);
+        var rb = projectile.GetComponent<Rigidbody>();
+        rb.linearVelocity = projectileSpeed * transform.forward;
+    }
+
+    void ShootIfClose()
+    {
+        float playerDistance = (player.position - transform.position).magnitude;
+
+        if (playerDistance < attackRange && AttackReady)
+        {
+            Shoot();
+        }
+    }
+
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Vector3 direction;
+
+    void Setup(Vector3 direction)
+    {
+        this.direction = direction;
+    }
+    void Update()
+    {
+
+    }
+}
