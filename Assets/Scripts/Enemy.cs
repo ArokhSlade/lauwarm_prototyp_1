14d25f9d0d9bd54e8b4675dc96827ef7f27ed9d1
Christian Schneider <fuzzyherbivore@icloud.com>
2025-06-12 14:59:09 +0200

    Made enemies shoot projectiles, aiming at boat.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,37 +1,61 @@
 ﻿using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject projectilePrefab;
     [SerializeField] float projectileSpeed;
-    [SerializeField] Transform player;
+    [SerializeField] Transform target;
 
     [SerializeField] float shootInterval;
     [SerializeField] float attackRange = 5f;
     float timeToShoot;
 
-    float AttackReady
+    void FixedUpdate()
+    {
+        ShootIfClose();
+    }
+
+    void Start()
+    {
+        timeToShoot = shootInterval;
+    }
+
+    bool AttackReady
     {
         get
         {
-            timeToShoot <= 0f;
+            if (timeToShoot <= 0f)
+            {
+                timeToShoot += shootInterval;
+                return true;
+            }
+
+            return false;
         }
     }
 
-    void Shoot(Vector3 direction)
+    void Shoot()
     {
         var projectile = Instantiate(projectilePrefab);
         var rb = projectile.GetComponent<Rigidbody>();
-        rb.linearVelocity = projectileSpeed * transform.forward;
+        rb.transform.position = transform.position;
+        rb.transform.LookAt(target);
+        rb.linearVelocity = projectileSpeed * rb.transform.forward;
     }
 
     void ShootIfClose()
     {
-        float playerDistance = (player.position - transform.position).magnitude;
+        float targetDistance = (target.position - transform.position).magnitude;
 
-        if (playerDistance < attackRange && AttackReady)
+        if (targetDistance < attackRange)
         {
-            Shoot();
+            timeToShoot -= Time.deltaTime;
+
+            if (AttackReady)
+            {
+                Shoot();
+            }
         }
     }
 
