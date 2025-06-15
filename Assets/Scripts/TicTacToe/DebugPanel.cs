bde1499119657626261534ee0f159d9c0991af9f
ArokhSlade <139560570+ArokhSlade@users.noreply.github.com>
2025-06-15 15:07:06 +0200

    added test ui to check winner. fixed logic errors.

---
+++ Assets/Scripts/TicTacToe/DebugPanel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace TicTacToe
+{
+    public class DebugPanel : MonoBehaviour
+    {
+        [SerializeField] TMPro.TMP_Text winnerLabel;
+        [SerializeField] Board board;
+        public void UpdateWinnerLabel()
+        {
+            FieldState winner = board.DetermineWinner();
+            switch (winner)
+            {
+                case FieldState.X:
+                    winnerLabel.text = "Winner = X";
+                    winnerLabel.color = Color.blue;
+                    break;
+                case FieldState.O:
+                    winnerLabel.text = "Winner = O";
+                    winnerLabel.color = Color.red;
+                    break;
+                case FieldState.Empty:
+                default:
+                    winnerLabel.text = "Winner = Nobody";
+                    winnerLabel.color = Color.yellow;
+                    break;
+            }
+        }
+    }
+}
