9a7808834bafbd3ca16f7f36d68e027570f3c74b
ArokhSlade <139560570+ArokhSlade@users.noreply.github.com>
2025-06-15 12:30:11 +0200

    check field whether it's already marked

--- Assets/Scripts/TicTacToe/Field.cs
+++ Assets/Scripts/TicTacToe/Field.cs
@@ -6,8 +6,24 @@ namespace TicTacToe
 {
     public class Field : MonoBehaviour
     {
+        // TODO(Gerald, 2025 06 15):
+        // synchronize marked-state with the existence of a corresponding child marker-object
+        // i.e. enforce single-source-of-truth / invariant
+        FieldState markedState = FieldState.Empty;
+
+        public bool IsMarked()
+        {
+            bool result = markedState != FieldState.Empty;
+
+            return result;
+        }
         public void Mark(FieldState state)
         {
+            if (IsMarked())
+            {
+                return;
+            }
+
             GameObject marker = null;
             FieldState markerType = FieldState.Empty;
 
@@ -25,6 +41,7 @@ namespace TicTacToe
             }
 
             marker = Markers.Instance.Create(markerType, this.transform);
+            markedState = state;
         }
     }
 }
