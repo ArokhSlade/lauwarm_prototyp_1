e9e9ed0029c284f65ee45a0f781ac86c5b0d2e5a
ArokhSlade <139560570+ArokhSlade@users.noreply.github.com>
2025-06-15 16:39:55 +0200

    tictactoe: moved field marking logic / authority from field to board

--- Assets/Scripts/TicTacToe/Game.cs
+++ Assets/Scripts/TicTacToe/Game.cs
@@ -3,8 +3,58 @@ using UnityEngine;
 
 namespace TicTacToe
 {
+
     public class Game : MonoBehaviour
     {
 
+        enum GameState
+        {
+            PreGame, Turn, GameOver
+        }
+
+        int playerCount = 2;
+        FieldState currentPlayer;
+        [SerializeField] Board board;
+        GameState state = GameState.PreGame;
+
+        void Start()
+        {
+            StartGame(FieldState.X);
+        }
+
+        void StartGame(FieldState firstPlayer)
+        {
+            currentPlayer = firstPlayer;
+            state = GameState.Turn;
+        }
+
+        public void FinishTurn()
+        {
+            FieldState winner = board.DetermineWinner();
+            if (winner != FieldState.Empty)
+            {
+                GameOver();
+            }
+            currentPlayer = NextPlayer();
+        }
+
+        FieldState NextPlayer()
+        {
+            FieldState result;
+            if (currentPlayer == FieldState.X)
+            {
+                result = FieldState.O;
+            } else
+            {
+                result = FieldState.X;
+            }
+
+            return result;
+        }
+
+        void GameOver()
+        {
+
+        }
     }
 }
