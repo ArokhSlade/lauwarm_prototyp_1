673cafb917799bff5f88e95ac7283c23953e17a1
Christian Schneider <fuzzyherbivore@icloud.com>
2025-06-17 14:39:17 +0200

    WIP: Refactoring of TicTacToe to use unity grid system.

---
+++ Assets/UserInput.cs
@@ -0,0 +1,28 @@
+using TicTacTwo;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[RequireComponent(typeof(Board))]
+public class UserInput : MonoBehaviour
+{
+    [SerializeField] LayerMask boardLayer;
+
+    Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton((int)MouseButton.LeftMouse))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if (!Physics.Raycast(ray, out RaycastHit hit, 100f, boardLayer)) return;
+
+            Debug.Log($"Click on board at world position {hit.point}");
+        }
+    }
+}
