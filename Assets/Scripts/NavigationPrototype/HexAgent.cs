5ec12e3c3acb8a17216a8ea3c252c0cc226771f4
ArokhSlade <139560570+ArokhSlade@users.noreply.github.com>
2025-06-22 17:12:31 +0200

    HexAgent: re-enabled OnDrawGizmos()

--- Assets/Scripts/NavigationPrototype/HexAgent.cs
+++ Assets/Scripts/NavigationPrototype/HexAgent.cs
@@ -52,19 +52,19 @@ namespace Navigation
             }
         }
 
-        //private void OnDrawGizmos()
-        //{
-            //if (debugRadius <= 0f)
-            //{
-            //    return;
-            //}
+        private void OnDrawGizmos()
+        {
+            if (debugRadius <= 0f)
+            {
+                return;
+            }
 
-            //Gizmos.color = Color.yellow;
-            //var positions = GetPathPositions();
-            //foreach (var position in positions)
-            //{
-            //    Gizmos.DrawSphere(position, debugRadius);
-            //}
-        //}
+            Gizmos.color = Color.yellow;
+            var positions = GetPathPositions();
+            foreach (var position in positions)
+            {
+                Gizmos.DrawSphere(position, debugRadius);
+            }
+        }
     }
 }
